01c0328d7a05ee53bcbd6b161c71afde62e1bf26
DmitrovD <92047119+DmitrovD@users.noreply.github.com>
2022-04-17 20:20:41 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,31 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// Задание 2
+// В HTML есть пустой список ul#ingredients.
+
+// Напиши скрипт, который для каждого элемента 
+// массива ingredients:
+
+// Создаст отдельный элемент < li >.Обзательно 
+// используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+
+
+const makeListIngredients = ingredients.map(ingrid => {
+  const newItem = document.createElement('li');
+  newItem.textContent = ingrid;
+
+  newItem.classList.add('item');
+
+  return newItem;
+});
+
+// После чего вставит все < li > за одну операцию в 
+// список ul.ingredients.
+
+const createItemIngredients = document.querySelector(`#ingredients`);
+createItemIngredients.append(...makeListIngredients);
